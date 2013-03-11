be032d24b8bb7d6a80fe003fa4ab40689fd71791
Jat <dpjat1@gmail.com>
2013-03-11 15:38:56 +0800

    删除附件大小限制

--- upload.php
+++ upload.php
@@ -18,12 +18,12 @@ $mw = intval($_GET['mw']);
 $rsp = array('status'=>201, 'msg'=>'ok');
 
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    if($_FILES['filetoupload']['size'] && $_FILES['filetoupload']['size'] < 1048576){
+    if($_FILES['filetoupload']['size']){
           // 上传的文件名
         $up_name = strtolower($_FILES['filetoupload']['name']);
         // 上传文件扩展名
         $ext_name = pathinfo($up_name, PATHINFO_EXTENSION);
-        
+
         if($options['ext_list']){
             // 如果限制扩展名
             if(in_array($ext_name, explode(",", $options['ext_list']))){
@@ -35,10 +35,10 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
         }else{
             $pass = '1';
         }
-        
+
         if($pass){
             $is_img = null;
-            
+
             // 尝试以图片方式处理
             $img_info = getimagesize($_FILES['filetoupload']['tmp_name']);
             if($img_info){
@@ -78,15 +78,15 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
                     }
                 }
             }
-            
-            
+
+
             ///保存
             $upload_dir = 'upload/'.$cur_uid;
             $upload_filename = $upload_dir.'/'.$new_name;
-            
+
             if($is_img){
                 // 是正确的图片文件
-                
+
                 // 判断是不是动态gif
                 $is_gifs = null;
                 if($img_info[2]==1){
@@ -95,7 +95,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
                         $is_gifs = '1';
                     }
                 }
-                
+
                 if(!$is_gifs){
                     if($img_info[0] > $mw){
                         $percent = $mw/$img_info[0];
@@ -105,11 +105,11 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
                         $new_w = $img_info[0];
                         $new_h = $img_info[1];
                     }
-                    
+
                     $new_image = imagecreatetruecolor($new_w, $new_h);
                     $bg = imagecolorallocate ( $new_image, 255, 255, 255 );
                     imagefill ( $new_image, 0, 0, $bg );
-                    
+
                     ////目标文件，源文件，目标文件坐标，源文件坐标，目标文件宽高，源宽高
                     imagecopyresampled($new_image, $img_obj, 0, 0, 0, 0, $new_w, $new_h, $img_info[0], $img_info[1]);
                     // 添加水印
@@ -122,13 +122,13 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
                     }
                 }
                 imagedestroy($img_obj);
-                
+
                 // 上传到云存储
                 include(dirname(__FILE__) . '/bcs.class.php');
                 $baidu_bcs =new BaiduBCS ( BCS_AK, BCS_SK, BCS_HOST );
-                
+
                 $bcs_object = '/'.$upload_filename;
-                
+
                 if(!$is_gifs){
                     ob_start();
                     imagejpeg($new_image, NULL, 95);
@@ -136,7 +136,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
                     ob_end_clean();
                     imagedestroy($new_image);
                 }
-                
+
                 try{
                     $response = (array)$baidu_bcs->create_object_by_content(BUCKET, $bcs_object, $out_img, array('acl'=>'public-read','contenttype'=>'image/jpeg'));
                     if($response['status']==200){
@@ -150,13 +150,13 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
                     $rsp['msg'] = '百度云存储创建对象失败，请稍后再试';
                 }
                 unset($out_img);
-                
+
             }else{
                 // 其它文件
                 // 上传到云存储
                 include(dirname(__FILE__) . '/bcs.class.php');
                 $baidu_bcs = new BaiduBCS ( BCS_AK, BCS_SK, BCS_HOST );
-                
+
                 $bcs_object = '/'.$upload_filename;
                 try{
                     $response = (array)$baidu_bcs->create_object(BUCKET, $bcs_object, $_FILES['filetoupload']['tmp_name'], array('acl'=>'public-read','contenttype'=>'image/jpeg'));
@@ -170,14 +170,14 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
                 }catch (Exception $e){
                     $rsp['msg'] = '百度云存储创建对象失败，请稍后再试';
                 }
-                
+
             }
         }
-        
+
     }else{
         $rsp['msg'] = '附件数据没有正确上传';
     }
-    
+
     header("Content-Type: text/html");
     echo json_encode($rsp);
 }
