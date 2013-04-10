d0d45afae39abb64dac6805c02ba4a035dc7e815
Jat <dpjat1@gmail.com>
2013-04-10 16:59:40 +0800

    url_path

--- common.php
+++ common.php
@@ -29,7 +29,7 @@ $mtime = explode(' ', microtime());
 $starttime = $mtime[1] + $mtime[0];
 $timestamp = time();
 $php_self = addslashes(htmlspecialchars($_SERVER['PHP_SELF'] ? $_SERVER['PHP_SELF'] : $_SERVER['SCRIPT_NAME']));
-$url_path = substr($php_self, 0,-4);
+$url_path = substr($php_self, 1,-4);
 
 include (dirname(__FILE__) . '/include/mysql.class.php');
 // 初始化从数据类，若要写、删除数据则需要定义主数据类
@@ -109,7 +109,7 @@ if(strpos($_SERVER["REQUEST_URI"], '.php')){
 
 // 只允许注册用户访问
 if($options['authorized'] && (!$cur_user || $cur_user['flag']<5)){
-    if( !in_array($url_path, array('/login','/logout','/sigin','/forgot','/qqlogin','/qqcallback','/qqsetname','/wblogin','/wbcallback','/wbsetname'))){
+    if( !in_array($url_path, array('login','logout','sigin','forgot','qqlogin','qqcallback','qqsetname','wblogin','wbcallback','wbsetname'))){
         header('location: /login');
         exit('authorized only');
     }
@@ -117,7 +117,7 @@ if($options['authorized'] && (!$cur_user || $cur_user['flag']<5)){
 
 // 网站暂时关闭
 if($options['close'] && (!$cur_user || $cur_user['flag']<99)){
-    if( !in_array($url_path, array('/login','/forgot'))){
+    if( !in_array($url_path, array('login','forgot'))){
         header('location: /login');
         exit('site close');
     }
--- templates/default/forgot.php
+++ templates/default/forgot.php
@@ -14,7 +14,7 @@ echo '</p>
 <p><label>登录名： <input type="text" name="name" class="sl w200" value="',htmlspecialchars($name),'" /></label>  <span class="fs12">允许字母、数字、中文，不能全为数字，4~12个字节</span></p>
 <p><label>邮　箱： <input type="text" name="email" class="sl w200" value="" /></label></p>
 <p><input type="submit" value="找回密码" name="submit" class="textbtn" style="margin-left:60px;" /> </p>';
-if($url_path == '/login'){
+if($url_path == 'login'){
     if($options['close_register'] || $options['close']){
         echo '<p class="grey fs12">网站暂时关闭 或 已停止新用户注册';
     }else{
