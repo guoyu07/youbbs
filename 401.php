8718d461b0d3e20a0c3bc211c7dac635276aa36a
Jat <dpjat1@gmail.com>
2013-04-29 17:02:11 +0800

    error pages

--- 401.php
+++ 401.php
@@ -5,13 +5,13 @@ header("Content-Type: text/html; charset=UTF-8");
 
 if ($error_code) {
     if ($error_code == 4011) {
-        $tips = '该账户还在审核中，请耐心等待。 <a href="/" style="color: red;">返回主页</a>';
+        $tips = '该账户还在审核中，请耐心等待。 <a href="/">返回主页</a>';
     }
     if ($error_code == 4012) {
-        $tips = '这个页面需要登录后才能访问，请使用管理员账户 <a href="/login" style="color: red;">登录</a> 或 <a href="/" style="color: red;">返回主页</a> 。';
+        $tips = '这个页面需要登录后才能访问，请使用管理员账户 <a href="/login">登录</a> 或 <a href="/">返回主页</a> 。';
     }
 } else {
-    $tips = '嗯~&nbsp;这个页面需要登录后才能访问哦，请 <a href="/login" style="color: red;">登录</a> 或 <a href="/" style="color: red;">返回主页</a> 。';
+    $tips = '嗯~&nbsp;这个页面需要登录后才能访问哦，请 <a href="/login">登录</a> 或 <a href="/">返回主页</a> 。';
 }
 
 echo '<!DOCTYPE html>
@@ -19,41 +19,23 @@ echo '<!DOCTYPE html>
     <head>
         <meta charset="utf-8" />
         <meta name="robots" content="none" />
-        <title>SinoSky 社区 - 401 Unauthorized!</title>
+        <title>401 Unauthorized!</title>
         <link type="text/css" rel="stylesheet" href="/static/error/error.css" charset="utf-8" />
-        <link rel="icon" type="image/png" href="/static/error/favicon.png" />
+        <link rel="icon" type="image/png" href="/favicon.ico" />
     </head>
     <body>
         <div class="wrapper">
             <div class="wrap">
                 <div class="content">
-                    <div class="logo">
-                        <a href="/" >
-                            <img border="0" width="153" height="56" alt="SinoSky" title="SinoSky - 自由 平等 开放 分享" src="/static/error/logo.jpg"/>
-                        </a>
-                    </div>
                     <div class="main">
                         <h2>401 Unauthorized!</h2>
-                        <br/><br/>
-                        <center>
-                            <img src="/static/error/error.png" alt="401: Unauthorized" title="401: Unauthorized" />
-                            <br/>
+                        <img src="/static/error/error.png" alt="401: Unauthorized" title="401: Unauthorized" />
                             <p>
                                 ', $tips, '
-
                             </p>
-                        </center>
-                        <a href="/" >
-                            <img border="0" class="icon" alt="SinoSky" title="SinoSky - 自由 平等 开放 分享" src="/static/error/favicon.png"/>
-                        </a>
                     </div>
                     <span class="clear"></span>
                 </div>
-            </div>
-        </div>
-        <div class="footerWrapper">
-            <div class="footer">
-                Copyright&nbsp;&nbsp;&copy;&nbsp;2012-2013 <a href="/" style="color: blue;">SinoSky</a>,&nbsp;All Rights Reserved.
             </div>
         </div>
     </body>
--- 404.php
+++ 404.php
@@ -7,24 +7,11 @@ echo '<!DOCTYPE html>
 <head>
     <meta charset="utf-8">
     <meta name="robots" content="none" />
-    <title>SinoSky 社区 - 404 Not Found!</title>
-    <link rel="stylesheet" href="/static/error/404.css">
-    <link rel="icon" type="image/png" href="/static/error/favicon.png" />
-    <style type="text/css"></style><style>@-moz-keyframes nodeInserted{from{opacity:0.99;}to{opacity:1;}}@-webkit-keyframes nodeInserted{from{opacity:0.99;}to{opacity:1;}}@-o-keyframes nodeInserted{from{opacity:0.99;}to{opacity:1;}}@keyframes nodeInserted{from{opacity:0.99;}to{opacity:1;}}embed,object{animation-duration:.001s;-ms-animation-duration:.001s;-moz-animation-duration:.001s;-webkit-animation-duration:.001s;-o-animation-duration:.001s;animation-name:nodeInserted;-ms-animation-name:nodeInserted;-moz-animation-name:nodeInserted;-webkit-animation-name:nodeInserted;-o-animation-name:nodeInserted;}</style>
+    <title>404 Not Found!</title>
+    <link rel="icon" type="image/png" href="/favicon.ico" />
 </head>
 <body>
-<div id="container">
-    <header id="header">
-        <a href="/" title="SinoSky - 自由 平等 开放 分享"><img alt="SinoSky" src="/static/error/logo-transparent.png"/></a>
-    </header>
-    <div id="main-wrap">
-        <img src="/static/error/404.png" alt="404: Not Found" title="404: Not Found">
-    </div>
-    <footer id="colophon">
-        <div class="border"></div>
-        <div class="copyright">Copyright &copy; 2012-2013 <a href="/">SinoSky</a>, All Rights Resverved.</div>
-    </footer>
-</div>
+<script type="text/javascript" src="http://www.qq.com/404/search_children.js" charset="utf-8"></script>
 </body>
 </html>
 ';
