058be2b043220beefd64e68b792fd8ff3543d868
Jat <dpjat1@gmail.com>
2013-05-28 05:03:04 +0800

    隐藏节点

--- index.php
+++ index.php
@@ -7,14 +7,14 @@ include_once(dirname(__FILE__) . '/common.php');
 // 获取最近文章列表
 $articledb = $MMC->get('home-article-list');
 if(!$articledb){
-    $hide_nodes_arr = explode(",", $options['hide_nodes']);
-    $hide_nodes_str = "AND cid <> ".implode(" AND cid <> ", $hide_nodes_arr);
+    if ($options['hide_nodes']) $hide_nodes_str = " AND cid <> ".str_replace(",", " AND cid <> ", $options['hide_nodes']);
+    else $hide_nodes_str = "";
     $query_sql = "SELECT a.id,a.cid,a.uid,a.ruid,a.title,a.addtime,a.edittime,a.comments,c.name as cname,u.avatar as uavatar,u.name as author,ru.name as rauthor
         FROM yunbbs_articles a
         LEFT JOIN yunbbs_categories c ON c.id=a.cid
         LEFT JOIN yunbbs_users u ON a.uid=u.id
         LEFT JOIN yunbbs_users ru ON a.ruid=ru.id
-        WHERE visible = 1 ".$hide_nodes_str."
+        WHERE visible = 1".$hide_nodes_str."
         ORDER BY edittime DESC LIMIT ".$options['home_shownum'];
     $query = $DBS->query($query_sql);
     $articledb=array();
